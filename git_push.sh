e84399cadff10c5a71049528e6e681c9fa4190f9
anandhegde <hegde.anand7@gmail.com>
2018-03-25 17:55:00 +0530

    file changed, commit on Sun Mar 25 17:55:00 IST 2018

--- git_push.sh
+++ git_push.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+!/bin/sh
 
 GIT=`which git`
 REPO_DIR=/home/anihegde/learn/shell
 cd ${REPO_DIR}
 ${GIT} add --all .
-${GIT} commit -m "Test commit"
+${GIT} commit -m "file changed, commit on $(date)"
 ${GIT} push git@github.com:anandhegde/directory_change.git master
